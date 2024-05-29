4f8778197b16300bf4918646010b1a12a2cd3cdc
Dr0pe29 <pedrosantosmacedo@gmail.com>
2024-05-29 18:33:31 +0100

    APP almost done

--- web/app.py
+++ web/app.py
@@ -11,7 +11,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://postgres:postgres@post
 pool = ConnectionPool(
     conninfo=DATABASE_URL,
     kwargs={
-        "autocommit": False,  # If True don’t start transactions automatically.
+        "autocommit": True,  # If True don’t start transactions automatically.
         "row_factory": namedtuple_row,
     },
     min_size=4,
@@ -59,3 +59,102 @@ def get_clinics():
             ).fetchall()
             
     return jsonify(clinicas)
+
+@app.route("/c/<clinica>/", methods=("GET",))
+def get_specialty(clinica):
+    """ Lists all specialties available in a clinic"""
+    with pool.connection() as conn:
+        with conn.cursor() as cur:
+            specialty = cur.execute(
+                """
+                SELECT DISTINCT m.especialidade
+                FROM medico m
+                JOIN trabalha t ON m.nif = t.nif
+                WHERE t.nome = %(clinica)s;
+                """,
+                {"clinica": clinica},
+            ).fetchall()
+    
+    if specialty is []:
+        return jsonify({"message": "Clinica não encontrada", "status": "error"}), 404
+    return jsonify(specialty)
+
+@app.route("/c/<clinica>/<especialidade>", methods=("GET",))
+def get_availability(clinica, especialidade):
+    """ Lists 3 available times for an apointment for each doctor"""
+    with pool.connection() as conn:
+        with conn.cursor() as cur:
+            availability = cur.execute(
+                """
+                WITH medicos_disponiveis AS (
+                    SELECT m.nome, m.nif
+                    FROM medico m
+                    JOIN trabalha t ON m.nif = t.nif
+                    WHERE m.especialidade = %(especialidade)s AND t.nome = %(clinica)s
+                )                         
+                SELECT DISTINCT (m.nome, ha.horario)
+                FROM medicos_disponiveis m 
+                CROSS JOIN LATERAL (
+                    SELECT horario
+                    FROM horario_aux ha
+                    WHERE NOT EXISTS (
+                        SELECT 1
+                        FROM consulta c
+                        WHERE c.nif = m.nif
+                        AND ha.horario = c.data + c.hora
+                    ) AND 
+                    horario > LOCALTIMESTAMP + INTERVAL '1 hour'
+                    AND EXISTS (
+                        SELECT 1
+                        FROM trabalha t
+                        WHERE t.nif = m.nif
+                        AND t.dia_da_semana = EXTRACT(DOW FROM ha.horario)
+                    )
+                    ORDER BY horario
+                    LIMIT 3
+                ) AS ha;
+                """,
+                {"especialidade": especialidade, "clinica": clinica},
+            ).fetchall()
+    
+    #if specialty is []:
+    #    return jsonify({"message": "Clinica não encontrada", "status": "error"}), 404
+    return jsonify(availability)
+
+
+
+
+@app.route("/a/<clinica>/registar", methods=("POST",))
+def register_apointment(clinica):
+    """ Registers an apointment in a clinic"""
+
+    paciente = request.args.get("Paciente SSN")
+    log.debug(paciente)
+    doutor = request.args.get("Médico NIF")
+    log.debug(doutor)
+    data = request.args.get("Data")
+    log.debug(data)
+    hora = request.args.get("Hora")
+    log.debug(hora)
+
+    #check_args(paciente, doutor, data, hora)
+
+    with pool.connection() as conn:
+        with conn.cursor() as cur:
+            apointment = cur.execute(
+                """
+                INSERT INTO consulta (ssn, nif, nome, data, hora)
+                VALUES (%(paciente)s,%(doutor)s,%(clinica)s,%(data)s,%(hora)s);
+                """,
+                {"paciente": paciente, "doutor": doutor, "clinica": clinica, "data":data, "hora":hora},
+            ).fetchall()
+    
+    return jsonify(apointment)
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+
---
+++ web/horarios.sql
@@ -0,0 +1,21 @@
+CREATE TABLE horario_aux(
+	horario TIMESTAMP PRIMARY KEY
+);
+
+INSERT INTO horarios_aux (horario)
+SELECT horario
+FROM (
+    SELECT generate_series(
+        '2024-01-01 08:00:00'::timestamp,
+        '2024-12-31 13:00:00'::timestamp,
+        '30 minutes'::interval
+    ) AS horario
+    UNION ALL
+    SELECT generate_series(
+        '2024-01-01 14:00:00'::timestamp,
+        '2024-12-31 19:00:00'::timestamp,
+        '30 minutes'::interval
+    ) AS horario
+) AS horarios
+WHERE EXTRACT(HOUR FROM horario) BETWEEN 8 AND 12
+   OR EXTRACT(HOUR FROM horario) BETWEEN 14 AND 18;
